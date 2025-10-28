620e95152a0845d4cae6692865bc99b369957a57
saaviasole <swapnaliasole@yahoo.com>
2025-10-28 17:06:47 +0530

    methods

---
+++ script.js
@@ -0,0 +1,8 @@
+var fruits = ["Mango","Apple","Banana","Strawbery"]
+console.log(fruits[0])
+
+console.log(fruits.join("*"))
+console.log(fruits.pop())
+console.log(fruits)
+fruits.push("guava")
+console.log(fruits)
