809705536711431b16533880a567c35f137b2190
saaviasole <swapnaliasole@yahoo.com>
2025-10-31 09:00:58 +0530

    testing

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var fruits = ["Mango","Apple","Banana","Strawbery"]
-console.log(fruits[0])
+ var fruits = ["Mango","Apple","Banana","Strawbery"]
+// console.log(fruits[0])
 
-console.log(fruits.join("*"))
-console.log(fruits.pop())
-console.log(fruits)
-fruits.push("guava")
-console.log(fruits)
+ //console.log(fruits.join("*"))
+
+ //console.log(fruits.pop())
+
+ // console.log(fruits)
+
+ //fruits.push(guava)
+
+// console.log(fruits)
+
